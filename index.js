41d855bbed0d733675f906174f0742d5651407f8
QuietMisdreavus <grey@quietmisdreavus.net>
2019-11-26 11:20:18 -0500

    factor into functions + take url from args

--- index.js
+++ index.js
@@ -9,15 +9,39 @@ const nodePandoc = require('node-pandoc');
 // node-pandoc is a function which takes a callback, but i want to use a promise
 const pandoc = util.promisify(nodePandoc);
 
-var testUrl = 'https://www.theguardian.com/technology/2019/nov/21/how-our-home-delivery-habit-reshaped-the-world';
+async function renderArticle(inputUrl) {
+    try {
+        var body = await request(inputUrl);
+        return await Mercury.parse(inputUrl, {
+            html: Buffer.from(body, "utf-8"),
+        });
+    } catch(err) {
+        throw err;
+    }
+}
 
-request(testUrl).then(function (body) {
-    return Mercury.parse(testUrl, {
-        html: Buffer.from(body, "utf-8"),
-    });
-}).then(function (result) {
-    var filename = `${result.title} [${result.domain}].docx`;
-    var args = ['-f', 'html', '-t', 'docx', '-o', filename];
-    console.log('saving file to', filename);
-    return pandoc(result.content, args);
+async function pandocToFile(article, filename) {
+    try {
+        filename = filename || `${article.title}.docx`;
+        var args = ['-f', 'html', '-t', 'docx', '-o', filename];
+        await pandoc(article.content, args);
+        return filename;
+    } catch(err) {
+        throw err;
+    }
+}
+
+if (process.argv.length <= 2) {
+    console.log('call this with a URL to fetch');
+    return;
+}
+
+var url = process.argv[2];
+
+renderArticle(url).then(function(result) {
+    return pandocToFile(result);
+}).then(function(filename) {
+    console.log('file written to:', filename);
+}).catch(function(err) {
+    console.log('ERROR:', err);
 });
